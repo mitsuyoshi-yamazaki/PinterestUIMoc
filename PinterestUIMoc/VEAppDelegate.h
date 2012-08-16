bf01f97e604c1683a3d25238cb3c716a504ea553
Yamazaki Mitsuyoshi <Mitsuyoshi@Mitsuyoshis-MacBook-Air.local>
2012-08-16 09:24:57 +0900

    Initial Commit

---
+++ PinterestUIMoc/VEAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VEAppDelegate.h
+//  PinterestUIMoc
+//
+//  Created by Mitsuyoshi Yamazaki on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VEAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PinterestUIMoc/VEViewController.h
@@ -0,0 +1,13 @@
+//
+//  VEViewController.h
+//  PinterestUIMoc
+//
+//  Created by Mitsuyoshi Yamazaki on 8/16/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VEViewController : UIViewController
+
+@end
